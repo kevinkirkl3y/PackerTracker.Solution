1f6741c1923136bd7f85d511c3877512bdf3e0f3
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-16 10:57:21 -0800

    purposefully passed test checking properties of Kit.
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

--- PackerTracker.Tests/ModelTests/KitTests.cs
+++ PackerTracker.Tests/ModelTests/KitTests.cs
@@ -10,8 +10,19 @@ namespace PackerTracker.Tests
     [TestMethod]
     public void KitConstructor_CreatesInstanceOfKit_Kit()
     {
-      Kit newKit = new Kit();
+      Kit newKit = new Kit("name", 5, 1, true, true);
       Assert.AreEqual(typeof(Kit), newKit.GetType());
+    }
+    
+    [TestMethod]
+    public void KitConstructor_ReturnsPropertiesOfKit_Properties()
+    {
+      Kit newKit = new Kit("flashslight", 5, 1, true, true);
+      Assert.AreEqual(name, newKit.Name);
+      Assert.AreEqual(5, newKit.Price);
+      Assert.AreEqual(1, newKit.Weight);
+      Assert.AreEqual(true , newKit.Purchased);
+      Assert.AreEqual(true, newKit.Packed);
     }
   }
 }
--- PackerTracker/Models/Kit.cs
+++ PackerTracker/Models/Kit.cs
@@ -1,8 +1,28 @@
+using System.Collections.Generic;
+
+
 namespace PackerTracker.Models
 {
     public class Kit
     {
-     
+        public string Name { get; set; }
+        public int Price { get; set; } 
+        public int Weight { get; set; }
+        public bool Purchased { get; set; }
+        public bool Packed { get; set; }
+        public int Id { get; set; }
+        private static List<Kit> _instances = new List<Kit> {};
+        public Kit (string name, int price, int weight, bool purchased, bool packed)
+        {
+          Name = name;
+          Price = price;
+          Weight = weight;
+          Purchased = purchased;
+          Packed = packed;
+          _instances.Add(this);
+          Id = _instances.Count;
+        }
 
+        
     }
 }
